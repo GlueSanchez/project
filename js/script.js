679c45f3df89fae800920cbbe43a11c1b7c2b38c
Oleksandr <visaszanew@gmail.com>
2020-08-20 20:08:02 +0300

    Marks for films

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('asdas0' == "asdas0");
+const numberOfFilms = +prompt('How much films do you see?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Last film?', ''),
+      b = prompt('Mark', ''),
+      c = prompt('Last film', ''),
+      d = prompt('Mark', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
