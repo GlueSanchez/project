dd640c7bac806569e84589d1ac531d6b7507f40e
Oleksandr <visaszanew@gmail.com>
2020-09-07 10:37:36 +0300

    Placed all functions in the object

--- js/script.js
+++ js/script.js
@@ -1,66 +1,71 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How much films do you see?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How much films do you see?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-};
 
-function rememberMyFilms() {
-    for (let i = 0; i < numberOfFilms; i++) {
-        const a = prompt('Last film?', ''),
-            b = prompt('Mark', '');
+    start: function () {
+        personalMovieDB.count = +prompt('How much films do you see?', '');
 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            i--;
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How much films do you see?', '');
         }
+    },
+
+    rememberMyFilms: function () {
+        for (let i = 0; i < personalMovieDB.count; i++) {
+            const a = prompt('Last film?', ''),
+                b = prompt('Mark', '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLever: function () {
+        if (personalMovieDB.count < 3) {
+            alert('Low level');
+        } else if (personalMovieDB.count >= 3 && personalMovieDB.count < 5) {
+            alert("Middle level");
+        } else if (personalMovieDB >= 7) {
+            alert("Top level");
+        } else {
+            console.log("Error");
+        }
+    },
+
+    showMyBD: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (genre === '' || genre == null) {
+                i--;
+                alert("BAD");
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i)=>{
+            console.log(`Genre ${i+1} - is ${item}`);
+        });
     }
-}
-
-rememberMyFilms();
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
-    }
-}
-
-writeYourGenres();
-
-function detectPersonalLever() {
-    if (personalMovieDB.count < 3) {
-        alert('Low level');
-    } else if (personalMovieDB.count >= 3 && personalMovieDB.count < 5) {
-        alert("Middle level");
-    } else if (personalMovieDB >= 7) {
-        alert("Top level");
-    } else {
-        console.log("Error");
-    }
-}
-
-detectPersonalLever();
-
-function showMyBD(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyBD(personalMovieDB.privat);
+};
